69b2b6bb11074f876822afe38d7fae08d197fa88
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2019-09-21 19:03:24 +0200

    Add files via upload

---
+++ cit_simulation_Meijer_et_al_2017.R
@@ -0,0 +1,16 @@
+set.seed(100)
+simulated_sds = c()
+for (i in 1:1000) {
+    all_preds = c()
+    for (i in 1:100) {
+        rands = rnorm(n = 5, mean = 0, sd = 1)
+        scaled_probe = (rands[1] - mean(rands)) / sd(rands)
+        all_preds = c(all_preds, scaled_probe)
+    }
+    simulated_sds = c(simulated_sds, sd(all_preds))
+}
+mean(simulated_sds)
+min(simulated_sds)
+max(simulated_sds)
+
+plot(density(all_preds))
